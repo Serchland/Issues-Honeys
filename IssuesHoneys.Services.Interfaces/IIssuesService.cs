53e497b194f1442b075b40bf24a9729e64a8c757
Serch00 <serchland.net@outlook.com>
2022-03-08 16:00:07 +0100

    Adding Service

---
+++ IssuesHoneys.Services.Interfaces/IIssuesService.cs
@@ -0,0 +1,16 @@
+﻿using IssuesHoneys.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IssuesHoneys.Services.Interfaces
+{
+    public interface IIssueService
+    {
+        List<Label> GetLabels();
+        List<Millestone> GetMillestones();
+        List<Issue> GetIssues();
+    }
+}
