53893028a5281b1c282e63da6c1487022644ad63
Serch00 <serchland.net@outlook.com>
2022-03-15 16:08:41 +0100

    UI

--- Modules/IssuesHoneys.Modules.Issues/ViewModels/AppDriveViewModel.cs
+++ Modules/IssuesHoneys.Modules.Issues/ViewModels/AppDriveViewModel.cs
@@ -8,7 +8,14 @@ namespace IssuesHoneys.Modules.Issues.ViewModels
     {
         public AppDriveViewModel(IApplicationCommands applicationCommands, IIssueService issueService) : base (applicationCommands, issueService)
         {
+            _dummyText = "LABELS";
+        }
 
+        private string _dummyText;
+        public string DummyText
+        {
+            get { return _dummyText; }
+            set { SetProperty(ref _dummyText, value); }
         }
     }
 }
