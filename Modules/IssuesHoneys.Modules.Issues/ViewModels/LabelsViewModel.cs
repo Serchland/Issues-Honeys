f062b536018dae776f9ca11ab160732ac67a70c3
Serch00 <serchland.net@outlook.com>
2022-03-17 21:08:16 +0100

    UI

--- Modules/IssuesHoneys.Modules.Issues/ViewModels/LabelsViewModel.cs
+++ Modules/IssuesHoneys.Modules.Issues/ViewModels/LabelsViewModel.cs
@@ -1,8 +1,27 @@
-﻿using Prism.Mvvm;
+﻿using IssuesHoneys.Core.Types.Interfaces;
+using Prism.Commands;
+using Prism.Mvvm;
 
 namespace IssuesHoneys.Modules.Issues.ViewModels
 {
     public class LabelsViewModel : BindableBase
     {
+        private IApplicationCommands _applicationCommands;
+
+        public LabelsViewModel(IApplicationCommands applicationCommands)
+        {
+            _applicationCommands = applicationCommands;
+        }
+
+        #region "Commands"
+        private DelegateCommand<string> _cancelCommand;
+        public DelegateCommand<string> CancelCommand =>
+            _cancelCommand ?? (_cancelCommand = new DelegateCommand<string>(ExecuteCommandName));
+
+        void ExecuteCommandName(string parameter)
+        {
+
+        }
+        #endregion
     }
 }
