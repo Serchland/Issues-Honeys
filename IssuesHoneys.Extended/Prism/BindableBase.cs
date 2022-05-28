910516c9c784d37fe0ff6bc1db08a9db677b3395
Serch00 <serchland.net@outlook.com>
2022-05-28 13:29:30 +0200

    BindeableBase staged

---
+++ IssuesHoneys.Extended/Prism/BindableBase.cs
@@ -0,0 +1,26 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace IssuesHoneys.Extended.Prism
+{
+    public abstract class BindableBase : INotifyPropertyChangedEnhanced
+    {
+        public event PropertyChangedEventHandlerEnhanced PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+
+            var oldValue = storage;
+            storage = value;
+            this.OnPropertyChanged(oldValue, value, propertyName);
+            return true;
+        }
+        protected void OnPropertyChanged<T>(T oldValue, T newValue, [CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedExtendedEventArgs(propertyName, oldValue, newValue));
+        }
+    }
+}
---
+++ IssuesHoneys.Extended/Prism/INotifyPropertyChangedEnhanced.cs
@@ -0,0 +1,9 @@
+﻿namespace IssuesHoneys.Extended.Prism
+{
+    public delegate void PropertyChangedEventHandlerEnhanced(object sender, PropertyChangedExtendedEventArgs e);
+
+    internal interface INotifyPropertyChangedEnhanced
+    {
+        event PropertyChangedEventHandlerEnhanced PropertyChanged;
+    }
+}
