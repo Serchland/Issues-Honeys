7586df2bab3a252ab3e2e4e0c02fc8ea71f3a6a7
Serch00 <serchland.net@outlook.com>
2022-05-28 13:31:47 +0200

    Revert "BindeableBase staged"
    This reverts commit 910516c9c784d37fe0ff6bc1db08a9db677b3395.

--- IssuesHoneys.Extended/Prism/INotifyPropertyChangedEnhanced.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace IssuesHoneys.Extended.Prism
-{
-    public delegate void PropertyChangedEventHandlerEnhanced(object sender, PropertyChangedExtendedEventArgs e);
-
-    internal interface INotifyPropertyChangedEnhanced
-    {
-        event PropertyChangedEventHandlerEnhanced PropertyChanged;
-    }
-}
